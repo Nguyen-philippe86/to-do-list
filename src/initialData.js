5f4b74a11f4b9609278f96fa6184eec35e8ade47
Nguyen <philippe.nguyen86@gmail.com>
2021-05-05 16:58:48 +0200

    final commit

---
+++ src/initialData.js
@@ -0,0 +1,8 @@
+let initialData = [
+    // {id: 'jsertu7a', name: 'Apprendre React.js', completed: false},
+    // {id: 'jseruo7l', name: 'Revoir Symfony', completed: false},
+    // {id: 'jseruy2q', name: 'Revoir PHP', completed: false},
+    // {id: 'jserv4sw', name: 'Revoir les évènement en JS', completed: false}
+]
+
+export default initialData;
